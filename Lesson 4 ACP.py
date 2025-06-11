e51acfbdcbeb8d3645aed11ebd60dd907c6c6e08
zaksamson777 <zaksamson777@gmail.com>
2025-06-11 17:44:30 +1200

    Add files via upload

---
+++ Lesson 4 ACP.py
@@ -0,0 +1,18 @@
+Weather= str(input('Enter the weather')) 
+
+Hot=input
+if Hot := input:
+    print('Wear T-shirt and Shorts')
+else :
+    print('Wear light clothes')
+Cold=input
+if Cold :=input:
+    print('Wear A jacket')
+else:
+    print('Wear warm clothes')
+
+
+
+ 
+
+
