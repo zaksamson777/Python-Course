07697719ed8a2d6953d1924b8105a856511b51a8
zaksamson777 <zaksamson777@gmail.com>
2025-09-01 19:12:49 +1200

    Add files via upload

---
+++ Lesson23ACP.py
@@ -0,0 +1,8 @@
+set1={"green","blue"}
+set2={"blue","yellow"}
+print("Original set elements:")
+print(set1)
+print(set2)
+print("\n symmetric diffrence of the two sets:")
+setz=set1.symmetric_difference(set2)
+print(setz)
